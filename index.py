4297aa6ba1ed5fee963e823a676697687585346c
jeanvetorello <jeanvetorello@gmail.com>
2022-02-23 09:24:30 -0300

    Update index.py
    ajust connect before login

--- index.py
+++ index.py
@@ -350,6 +350,9 @@ def login():
         login_attempts = login_attempts + 1
         #TODO mto ele da erro e poco o botao n abre
         # time.sleep(10)
+    if clickBtn(images['connect'], timeout = 10):
+        logger('🎉 Connect metamesk before login detected, logging in!')
+        login_attempts = login_attempts + 1    
 
     if clickBtn(images['select-wallet-2'], timeout=8):
         # sometimes the sign popup appears imediately
